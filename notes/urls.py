520fd7b7a31f392ffd27ce0bbad876e32be81986
emmanuella <emmavduru@gmail.com>
2024-03-04 20:28:15 +0000

    adjusted note delete url

--- notes/urls.py
+++ notes/urls.py
@@ -4,6 +4,6 @@ from .views import NoteListView, NoteCreateView, NoteUpdateView, NoteDeleteView
 urlpatterns = [
     path("create/", NoteCreateView.as_view(), name="note_create"),
     path("<int:pk>/edit/", NoteUpdateView.as_view(), name="note_edit"),
-    path("<int:pk>/delete", NoteDeleteView.as_view(), name="note_delete"),
+    path("<int:pk>/delete/", NoteDeleteView.as_view(), name="note_delete"),
     path("", NoteListView.as_view(), name="note_list")
 ]
