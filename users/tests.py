407ac1cd3218c66e63eff9f5e3e67149109be704
emmanuella <emmavduru@gmail.com>
2024-03-04 20:27:04 +0000

    added tests for the users app

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+from django.urls import reverse
+
+from .models import CustomUser
 
 # Create your tests here.
+class UserTests(TestCase):
+    
+    def setUp(self):
+        self.user = CustomUser.objects.create(
+            username="testuser",
+            email="test@email.com",
+            password="secret"
+        )
+        self.client = Client()
+
+    def test_string_representation(self):
+        user = CustomUser(
+            username="test",
+            email="testuser@email.com",
+            password="secret"
+        )
+        self.assertEqual(str(user), user.username)
+
+    def test_user_content(self):
+        self.assertEqual(f"{self.user.username}", "testuser")
+        self.assertEqual(f"{self.user.email}", "test@email.com")
+
+    
